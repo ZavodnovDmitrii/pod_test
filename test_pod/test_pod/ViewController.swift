68d3b4831598a89f3a6b82cccf62e6fff8891279
ZavodnovDmitrii <50870135+ZavodnovDmitrii@users.noreply.github.com>
2024-11-13 20:15:42 +0300

    add main label

--- test_pod/test_pod/ViewController.swift
+++ test_pod/test_pod/ViewController.swift
@@ -12,8 +12,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let label = UILabel()
+        label.text = "Main screen"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 30, weight: .heavy)
+        label.backgroundColor = .red
+        label.frame = .init(origin: .zero, size: .init(width: view.frame.width, height: 200))
+        view.addSubview(label)
     }
-
-
 }
 
